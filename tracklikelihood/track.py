d0ade380c2efeca235c0b64cdc2a89e4af69c9e7
MCFlowMace <flthomas@students.uni-mainz.de>
2020-07-28 19:54:08 +0200

    Add integration for the track signal

--- tracklikelihood/track.py
+++ tracklikelihood/track.py
@@ -23,6 +23,8 @@
 #  
 
 import numpy as np
+from scipy import special
+from math import sqrt
 
 def line(m, b, x):
     return m*x + b
@@ -35,7 +37,7 @@ def line_segment(t_start, t_end, f_start, f_end):
     m = df/dt
     b = f_end - m*t_end
     
-    return lambda x: line(m, b, x)
+    return lambda x: line(m, b, x), m
 
 def get_b_m(t_start, t_end, f_start, f_end):
     
@@ -49,8 +51,28 @@ def get_b_m(t_start, t_end, f_start, f_end):
 
 def gauss(y, mu, sigma):
     
-    return 1/(sigma*np.sqrt(2*np.pi))*np.exp(-(y-mu)**2/sigma)
-
+    return 1/(sigma*np.sqrt(2*np.pi))*np.exp(-1/2*((y-mu)/sigma)**2)
+    
+def erf_cont(diff, sigma):
+	#diff = mu - y
+	return diff*special.erf(diff/(sqrt(2)*sigma))
+	
+def exp_cont(diff, sigma):
+	return np.exp(-1/2*(diff/sigma)**2)
+	
+def exp_erf_cont(y, mu, sigma):
+	
+	diff = mu - y
+	return sqrt(2/np.pi)*sigma*exp_cont(diff, sigma) + erf_cont(diff, sigma)
+	
+def integrate_bin(mumin, mumax, ymin, ymax, sigma, slope):
+	
+    return 1/(2*slope) * (exp_erf_cont(ymax, mumin, sigma) \
+                    + exp_erf_cont(ymin, mumax, sigma) \
+                    - exp_erf_cont(ymax, mumax, sigma) \
+                    - exp_erf_cont(ymin, mumin, sigma))
+	
+	
 class Track:
     
     def __init__(self, t_start, t_end, f_start, f_end, sigma, snr):
@@ -80,20 +102,45 @@ class Track:
         
         return np.sqrt(df**2 + dt**2)
         
+    def get_integration_region(self, x, y, dx, dy):
+	
+        ymin = y-dy/2
+        ymax = y+dy/2
+        xmin = x-dx/2
+        xmax = x+dx/2
+
+        xmax = np.minimum(xmax, self.t_end)
+        xmin = np.maximum(xmin, self.t_start)
+
+        mu, _ = line_segment(self.t_start, self.t_end, self.f_start, self.f_end)
+
+        mumin = mu(xmin)
+        mumax = mu(xmax)
+
+        return ymin, ymax, mumin, mumax
+        
     def get_signal(self, t, f):
 
         track_signal = np.zeros(shape=[t.shape[0], f.shape[0]])
 
         track_ind = (t>(self.t_start))&(t<(self.t_end))
+        
+        dt = t[1]-t[0]
+        df = f[1]-f[0]
 
         t_track = t[track_ind]
 
         ff, tt = np.meshgrid(f, t_track)
+        
+        fmin, fmax, mumin, mumax = self.get_integration_region(tt, ff, dt, df)
 
-        mu = line_segment(self.t_start, self.t_end, self.f_start, self.f_end)
+        #mu, _ = line_segment(self.t_start, self.t_end, self.f_start, self.f_end)
 
-        track_signal[track_ind] = gauss(ff, mu(tt), self.sigma)
-
+        track_signal[track_ind] = integrate_bin(mumin, mumax, fmin, fmax, 
+                                                            self.sigma, 
+                                                            self.get_slope())  #gauss(ff, mu(tt), self.sigma)
+        track_signal[track_ind]/=dt*df
+        
         return self.snr*track_signal
     
     @classmethod

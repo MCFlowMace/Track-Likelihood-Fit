dfddac185c928f686f80ffc918b1a41c16d98497
Florian Thomas <flthomas@students.uni-mainz.de>
2020-08-26 22:40:26 +0200

    Modify the track hypothesis with sinc leakage

--- tracklikelihood/spectrogram.py
+++ tracklikelihood/spectrogram.py
@@ -2,25 +2,25 @@
 # -*- coding: utf-8 -*-
 #
 #  spectrogram.py
-#  
+#
 #  Copyright 2020 Florian Thomas <flthomas@students.uni-mainz.de>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+#
+#
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -28,25 +28,25 @@ from . import track
 from scipy.stats import expon
 
 def get_aspect(extent, shape):
-    
+
     x_res = (extent[0] - extent[1])/shape[0]
     y_res = (extent[2] - extent[3])/shape[1]
-    
+
     return x_res/y_res
-    
+
 def plot_spectrogram(spec, t, f, tracks=[], vscale='default', log_scale=False,
                                 name='spectrogram', save=False):
 
-    fig, ax = plt.subplots()  
-    
+    fig, ax = plt.subplots()
+
     dt = t[1]-t[0]
-    df = f[1]-f[0]  
+    df = f[1]-f[0]
 
     extent = [t[0], t[-1]+dt, f[0]-df/2, f[-1]+df/2]
     aspect = get_aspect(extent, spec.shape)
-    
+
     data = np.transpose(spec)
-    
+
     if log_scale:
         data = np.log(data)
 
@@ -57,27 +57,27 @@ def plot_spectrogram(spec, t, f, tracks=[], vscale='default', log_scale=False,
         print(vscale)
         vmin = vscale[0]
         vmax = vscale[1]
-    
-    cmap = ax.imshow(data, extent=extent, origin='lower', vmin=vmin, vmax=vmax, 
+
+    cmap = ax.imshow(data, extent=extent, origin='lower', vmin=vmin, vmax=vmax,
                                                     aspect=aspect)
-                                            
+
     for tr in tracks:
         t_start = tr[0]
         f_start = tr[1]
         t_end = tr[2]
         f_end = tr[3]
-        
+
         f_track, _ = track.line_segment(t_start, t_end, f_start, f_end)
-    
+
         t_probe = np.linspace(t_start, t_end, t.shape[0]*100)
         f_probe = f_track(t_probe)
         ax.plot(t_start, f_start, c='r', marker='x', markersize=2)
         ax.plot(t_end, f_end, c='r', marker='x', markersize=2)
         ax.plot(t_probe, f_probe, c='g', linewidth=1)
-    
+
     cbar = fig.colorbar(cmap)
     cbar.set_label('Power')
-    
+
     ax.set_xlabel('t[s]')
     ax.set_ylabel('f[Hz]')
 
@@ -85,17 +85,17 @@ def plot_spectrogram(spec, t, f, tracks=[], vscale='default', log_scale=False,
         plt.savefig(name+'.png', dpi=600)
 
     plt.show()
-    
+
 def plot_power_distribution(spec, name='power_dist', bins=50, save=False):
-	
-    fig, ax = plt.subplots()    
-    
+
+    fig, ax = plt.subplots()
+
     flat_spec = spec.flatten()
     maximum = np.max(flat_spec)
-    
+
     ax.hist(flat_spec, bins=bins, density=True)
     #ax.plot(expon.pdf(np.linspace(0,maximum, 1000)))
-    
+
     ax.set_xlabel('power')
     ax.set_ylabel('count')
 
@@ -105,8 +105,8 @@ def plot_power_distribution(spec, name='power_dist', bins=50, save=False):
     plt.show()
 
 class Spectrogram:
-	
-		
+
+
     def __init__(self, spec, t, f, name='spectrogram'):
         self.spec = spec
         self.t = t
@@ -116,81 +116,81 @@ class Spectrogram:
     @property
     def name(self):
         return self._name
-    
+
     @name.setter
     def name(self, name):
         if isinstance(name, str):
             self._name = name
-        else: 
+        else:
             raise TypeError('Expected a string')
-    
+
     @property
     def spec(self):
         return self._spec
-    
+
     @spec.setter
     def spec(self, spec):
         if isinstance(spec, np.ndarray) and spec.ndim==2:
             self._spec = spec
-        else: 
+        else:
             raise TypeError('Expected numpy array with ndim==2')
-            
+
     @property
     def t(self):
         return self._t
-    
+
     @t.setter
     def t(self, t):
         if isinstance(t, np.ndarray) and t.ndim==1:
             self._t = t
-        else: 
+        else:
             raise TypeError('Expected numpy array with ndim==1')
-            
+
     @property
     def f(self):
         return self._f
-    
+
     @f.setter
     def f(self, f):
         if isinstance(f, np.ndarray) and f.ndim==1:
             self._f = f
-        else: 
+        else:
             raise TypeError('Expected numpy array with ndim==1')
-            
+
     @classmethod
-    def from_tracks(cls, t, f, tracks, name='spectrogram', add_noise=True):
-		
+    def from_tracks(cls, t, f, tracks, N, BW, name='spectrogram', add_noise=True):
+
         signal = np.zeros((t.shape[0], f.shape[0]))
 
         for track in tracks:
-            signal += track.get_signal(t,f)
-            
+            signal += track.get_signal(t,f, N, BW)
+
         if add_noise:
             signal += np.random.exponential(1, (t.shape[0], f.shape[0]))
 
         return cls(signal, t, f, name)
-		
+
     def plot(self, tracks=[], vscale='default', log_scale=False, save=False):
-        
-        plot_spectrogram(self.spec, self.t, self.f, 
+
+        plot_spectrogram(self.spec, self.t, self.f,
                                 tracks, vscale, log_scale, self.name, save)
-        
+
     def normalize(self):
-		
+
         mean = np.mean(self.spec)
         self.spec /= mean
-		
+
     def power_dist(self, save=False):
-        
+
         plot_power_distribution(self.spec, name=self.name+"_power_dist", save=save)
 
 def main(args):
-	
+
     t = np.linspace(0, 50, 100)
     f = np.linspace(10, 200, 100)
     sp = Spectrogram.from_tracks(t, f, [])
     sp.plot()
-	
+
     return 0
 
 if __name__ == '__main__':
--- tracklikelihood/track.py
+++ tracklikelihood/track.py
@@ -2,25 +2,25 @@
 # -*- coding: utf-8 -*-
 #
 #  track.py
-#  
+#
 #  Copyright 2020 Florian Thomas <flthomas@students.uni-mainz.de>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+#
+#
 
 import numpy as np
 from scipy import special
@@ -30,51 +30,62 @@ def line(m, b, x):
     return m*x + b
 
 def line_segment(t_start, t_end, f_start, f_end):
-    
+
     dt = t_end - t_start
     df = f_end - f_start
-    
+
     m = df/dt
     b = f_end - m*t_end
-    
+
     return lambda x: line(m, b, x), m
 
 def get_b_m(t_start, t_end, f_start, f_end):
-    
+
     dt = t_end - t_start
     df = f_end - f_start
-    
+
     m = df/dt
     b = f_end - m*t_end
-    
+
     return b, m
 
 def gauss(y, mu, sigma):
-    
+
     return 1/(sigma*np.sqrt(2*np.pi))*np.exp(-1/2*((y-mu)/sigma)**2)
-    
+
 def erf_cont(diff, sigma):
 	#diff = mu - y
 	return diff*special.erf(diff/(sqrt(2)*sigma))
-	
+
 def exp_cont(diff, sigma):
 	return np.exp(-1/2*(diff/sigma)**2)
-	
+
 def exp_erf_cont(y, mu, sigma):
-	
+
 	diff = mu - y
 	return sqrt(2/np.pi)*sigma*exp_cont(diff, sigma) + erf_cont(diff, sigma)
-	
+
 def integrate_bin(mumin, mumax, ymin, ymax, sigma, slope):
-	
+
     return 1/(2*slope) * (exp_erf_cont(ymax, mumin, sigma) \
                     + exp_erf_cont(ymin, mumax, sigma) \
                     - exp_erf_cont(ymax, mumax, sigma) \
                     - exp_erf_cont(ymin, mumin, sigma))
-	
-	
+
+def rect_spectrum(f, M):
+
+    res = np.zeros(f.shape)
+
+    res[f==0] = M
+    res[f!=0] = np.sin(np.pi*f[f!=0]*M/2)/np.sin(np.pi*f[f!=0])
+
+    return res**2
+
+def f_frac(f, SR):
+    return f/SR
+
 class Track:
-    
+
     def __init__(self, t_start, t_end, f_start, f_end, sigma, snr):
         self.t_start = t_start
         self.t_end = t_end
@@ -82,28 +93,28 @@ class Track:
         self.f_end = f_end
         self.sigma = sigma
         self.snr = snr
-    
+
     def get_delta(self):
-        
+
         dt = self.t_end - self.t_start
         df = self.f_end - self.f_start
-        
+
         return dt, df
-    
+
     def get_slope(self):
-        
+
         dt, df = self.get_delta()
 
         return df/dt
-    
+
     def get_length(self):
 
         dt, df = self.get_delta()
-        
+
         return np.sqrt(df**2 + dt**2)
-        
+
     def get_integration_region(self, x, y, dx, dy):
-	
+
         ymin = y-dy#-dy/2
         ymax = y+dy#/2
         xmin = x#-dx/2
@@ -111,7 +122,7 @@ class Track:
 
         xmax = np.minimum(xmax, self.t_end)
         xmin = np.maximum(xmin, self.t_start)
-        
+
         #print(xmin[:,0], xmax[:,0])
 
         mu, _ = line_segment(self.t_start, self.t_end, self.f_start, self.f_end)
@@ -120,9 +131,9 @@ class Track:
         mumax = mu(xmax)
 
         return ymin, ymax, mumin, mumax
-        
-    def get_signal(self, t, f):
-		
+
+    def get_signal(self, t, f, N, BW):
+
         dt = (t[1]-t[0])
         df = (f[1]-f[0])/2
 
@@ -130,33 +141,45 @@ class Track:
       #  track_signal = -100*track_signal
 
         track_ind = (t>(self.t_start-dt))&(t<self.t_end)
-        
+
       #  first = np.argmax(t>self.t_start)
       #  print(first, t[first])
 
         t_track = t[track_ind]
 
         ff, tt = np.meshgrid(f, t_track)
-        
+
         fmin, fmax, mumin, mumax = self.get_integration_region(tt, ff, dt, df)
 
         #mu, _ = line_segment(self.t_start, self.t_end, self.f_start, self.f_end)
 
-        track_signal[track_ind] = integrate_bin(mumin, mumax, fmin, fmax, 
-                                                            self.sigma, 
-                                                            self.get_slope())  #gauss(ff, mu(tt), self.sigma)
-        
-        track_signal[track_ind]/=dt*df
-        
+       # track_signal[track_ind] = integrate_bin(mumin, mumax, fmin, fmax,
+       #                                                     self.sigma,
+       #                                                     self.get_slope())  #gauss(ff, mu(tt), self.sigma)
+
+        #track_signal[track_ind]/=dt*df
+
+        mean_f = (mumax+mumin)/2
+
+        track_signal[track_ind] = rect_spectrum((ff-mean_f)/BW, N)
+
+        #t_in0 = t_track[1]-self.t_start
+        #t_in1 = self.t_end-t_track[-1]
+
+       # print(t_in0, t_in1, dt, t_in0/dt, t_in1/dt)
+
+        #track_signal[track_ind[0]] *= t_in0/dt
+        #track_signal[track_ind[-1]] *= t_in1/dt
+
         return self.snr*track_signal
-    
+
     @classmethod
     def from_slope_and_length(cls, t_start, f_start, slope, length, sigma, snr):
-        
+
         dt = length
         df = dt*slope
         return cls(t_start, t_start+dt, f_start, f_start+df, sigma, snr)
-        
+
 
 def main(args):
     return 0
